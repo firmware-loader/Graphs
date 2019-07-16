e0bafb630d0d198892a1982b055cbb67b1f84729
Unknown <19Sebastian95@gmx.de>
2019-07-16 17:13:34 +0200

    new data + plots

--- e.R
+++ e.R
@@ -1,22 +1,48 @@
 library(ggplot2)
+library(plotly)
+library(gapminder)
+library(ggridges)
 
-elm <- `complete`
+timing <- `complete`
+timer <- `complete_timer`
 
-ggplot(elm[which(elm$V2>0),], aes(V3, V1)) + 
-  geom_smooth(aes(color = V2)) 
 
-ggplot(elm[which(elm$V2>0),], aes(V3, V1)) + 
+ggplot(timing, aes(V3, V1)) + 
   geom_col(aes(fill = V2))
 
-ggplot(elm[which(elm$V2>0),], aes(V3, V1)) + 
-  geom_point(aes(color = V2)) + 
-#  geom_smooth(aes(color = V2)) +
-  geom_smooth(se = FALSE, method = "gam", formula = y ~ s(log(x), bs = "cs")) 
 
-ggplot(data = elm[which(elm$V2>0),], aes(x = V3, y = V1, color = V2, weight = V1 * V2)) + 
+
+ggplot(timing[which(timing$V2==10),], aes(x=as.factor(V3), y=V1)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) + 
+  xlab("Frequenz") + 
+  ylab("Baud")
+
+ggplot(data = timing[which(timing$V2>0),], aes(x = V3, y = V1, color = V2, weight = V1 * V2)) + 
   geom_point() + 
-  #  geom_smooth(aes(color = V2)) +
   geom_smooth(method="lm", se = FALSE, legend=FALSE) + 
-  scale_color_gradient(low = "#004D40", high = "#FFC107")
+  scale_color_gradient() + 
+  xlab("Frequenz [Mhz]") + 
+  ylab("Baud")
 
-attach(elm)
+ggplot(data = timing[which(timing$V2>0),], aes(x = V3, y = V1, color = V2, weight = V1 * V2)) + 
+  geom_point() + 
+  geom_smooth(method="lm", se = FALSE, legend=FALSE) + 
+  scale_color_gradient() + 
+  xlab("Frequenz [Mhz]") + 
+  ylab("Baud")
+
+ggplot(timer[which(timer$V2==10),], aes(x=as.factor(V3), y=V1)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) + 
+  xlab("Frequenz") + 
+  ylab("Baud")
+
+ggplot(data = timer[which(timer$V2>0),], aes(x = V3, y = V1, color = V2, weight = V1 * V2)) + 
+  geom_point() + 
+  geom_smooth(method="auto", se = FALSE, legend=FALSE) + 
+  scale_color_gradient() + 
+  xlab("Taktrate des Mikrocontrollers [Mhz]") + 
+  ylab("‹bertragungsgeschwindigkeit [Baud]") + 
+  labs(title = "Erfolreiche ‹bertragungen in Abh‰nigkeit von ‹bertragunsgeschwindigkeiten und Taktraten", color = "Erfolgreiche\n‹bertragungen [%]")
+
+attach(timing)
+attach(timer)
